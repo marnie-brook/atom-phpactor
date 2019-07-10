c264b5719138313d65738ce8ab5efeacd602166a
William Brook <william.g.brook@gmail.com>
2019-07-10 12:25:37 +0100

    Initial commit supporting some Phpactor actions
    master
    
    Include basic support for interacting with phpactor
    from within the atom text editor

---
+++ spec/atom-phpactor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPhpactorView from '../lib/atom-phpactor-view';
+
+describe('AtomPhpactorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
